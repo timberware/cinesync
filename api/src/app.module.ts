c40ad542195678471210809ea01165c9b3df84ba
Chris Pinkney <hey@chrispinkney.com>
2023-09-13 12:35:34 -0400

    feat(api): implement prisma service and schema

--- api/src/app.module.ts
+++ api/src/app.module.ts
@@ -1,12 +1,14 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 import { ListsModule } from './lists/lists.module';
+import { PrismaService } from './prisma/prisma.service';
 
 @Module({
-	imports: [UsersModule, ListsModule],
+	imports: [ConfigModule.forRoot(), UsersModule, ListsModule],
 	controllers: [AppController],
-	providers: [AppService],
+	providers: [AppService, PrismaService],
 })
 export class AppModule {}
