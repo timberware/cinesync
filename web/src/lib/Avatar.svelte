7d572de7f5a5f999bbb6e05900378dc433541fbc
Josue <manekenpix@fastmail.com>
2024-04-23 22:04:59 -0400

    refactor(web): change the Avatar component to use initials or images if avaialble

--- web/src/lib/Avatar.svelte
+++ web/src/lib/Avatar.svelte
@@ -1,16 +1,48 @@
 <script lang="ts">
+  import { PUBLIC_API_HOST } from '$env/static/public';
+  import { onMount } from 'svelte';
   export let username: string;
   export let isLarge: Boolean = false;
 
   const initial = username[0];
+  let loaded = false;
+  let failed = false;
+  let loading = false;
+
+  const imageClass = `inline-block text-center rounded-full bg-accent text-text select-none ${
+    isLarge ? 'h-24 w-24 text-8xl' : 'h-6 w-6 has-tooltip'
+  }`;
+  const fullPath = `${PUBLIC_API_HOST || 'http://localhost:4000'}/images/${username}`;
+
+  onMount(() => {
+    const img = new Image();
+    img.src = fullPath;
+    loading = true;
+
+    img.onload = () => {
+      loading = false;
+      loaded = true;
+    };
+    img.onerror = () => {
+      loading = false;
+      failed = true;
+    };
+  });
 </script>
 
-<div
-  class="{`inline-block text-center rounded-full bg-accent text-text select-none ${
-    isLarge ? 'h-24 w-24 text-8xl' : 'h-6 w-6 has-tooltip'
-  }`}"
->
-  <span class="tooltip rounded-lg bg-background text-text p-2 mt-5 -ml-5">{username}</span
-  >
-  {initial}
-</div>
+{#if loaded}
+  <img class="{imageClass}" src="{fullPath}" alt="Avatar" />
+{:else if failed}
+  <div class="{imageClass}">
+    <span class="tooltip rounded-lg bg-background text-text p-2 mt-5 -ml-5"
+      >{username}</span
+    >
+    {initial}
+  </div>
+{:else if loading}
+  <img
+    class="{imageClass}"
+    src="https://c.tenor.com/On7kvXhzml4AAAAi/loading-gif.gif"
+    alt="Loading..."
+  />
+{/if}
