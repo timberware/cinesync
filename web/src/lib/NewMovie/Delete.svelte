cedb5a31c37cc7c0eb294d8c9a6286f884e5fc28
Josue <manekenpix@fastmail.com>
2024-04-28 16:14:51 -0400

    feat(web): add new movie component

---
+++ web/src/lib/NewMovie/Delete.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+  import { faTrash } from '@fortawesome/free-solid-svg-icons';
+  import SubmitButton from '$lib/SubmitButton.svelte';
+
+  export let listId: string;
+  export let movieId: string;
+</script>
+
+<SubmitButton
+  formAction="lists?/deleteMovie"
+  inputs="{[
+    { name: 'listId', value: listId },
+    { name: 'movieId', value: movieId }
+  ]}"
+  icon="{faTrash}"
+  tooltip="delete movie"
+/>
---
+++ web/src/lib/NewMovie/MovieContainer.svelte
@@ -0,0 +1,3 @@
+<div class="w-1/2 h-96 relative flex rounded-xl bg-secondary p-3">
+  <slot />
+</div>
