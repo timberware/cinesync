ba8286294a410345aab3b260c3b998e579175161
Chris Pinkney <hey@chrispinkney.com>
2024-05-07 11:29:37 -0400

    feat(api): add shared list filter

--- api/src/user/dao/user.dao.ts
+++ api/src/user/dao/user.dao.ts
@@ -165,7 +165,6 @@ export class UserDao {
                 list: {
                   where: {
                     NOT: { creatorId: userId },
-                    user: { some: { id: userId } },
                   },
                 },
               },
