1a588b048e16cd7900803b5ed57bf0726cf7a7f2
Josue <manekenpix@fastmail.com>
2024-05-23 21:54:11 -0400

    chore(web): update cinesync url

--- web/src/utils/urls.ts
+++ web/src/utils/urls.ts
@@ -6,7 +6,7 @@ export const searchMovieUrl = (movie: string) =>
 export const getPosterUrl = (posterUrl: string) =>
   `https://image.tmdb.org/t/p/w500${posterUrl}`;
 
-export const CINESYNC_REPO = 'https://github.com/funnls/cinesync';
+export const CINESYNC_REPO = 'https://github.com/chorizoft/cinesync';
 export const THE_MOVIE_DB = 'https://www.themoviedb.org/';
 export const THE_MOVIE_DB_AVATAR =
   'https://www.themoviedb.org/assets/2/v4/logos/v2/blue_square_1-5bdc75aaebeb75dc7ae79426ddd9be3b2be1e342510f8202baf6bffa71d7f5c4.svg';
