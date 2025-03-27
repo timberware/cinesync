394937a2ddfa96377373cf34840e710aa9c3e097
Josue <manekenpix@fastmail.com>
2025-03-26 22:32:10 -0400

    refactor(api): move gh stuff to be

--- api/src/app.service.ts
+++ api/src/app.service.ts
@@ -1,8 +1,31 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, InternalServerErrorException } from '@nestjs/common';
+import { Octokit } from '@octokit/rest';
+import { ConfigService } from '@nestjs/config';
 
 @Injectable()
 export class AppService {
-  getHello() {
-    return { hello: 'world' };
+  private oct: Octokit;
+  private GITHUB_TOKEN: string | undefined;
+
+  constructor(private configService: ConfigService) {
+    this.GITHUB_TOKEN = this.configService.get<string>('GITHUB_TOKEN');
+
+    if (!this.GITHUB_TOKEN)
+      throw new InternalServerErrorException('Env var GITHUB_TOKEN is missing');
+
+    this.oct = new Octokit({ auth: this.GITHUB_TOKEN });
+  }
+
+  async getVersion() {
+    const { data } = await this.oct.rest.repos.listTags({
+      owner: 'timberware',
+      repo: 'cinesync',
+    });
+
+    const webTags = data.filter((tag) => tag.name.includes('web'));
+
+    return {
+      tag: webTags.length ? webTags[0].name.replace('web-', '') : ':(',
+    };
   }
 }
